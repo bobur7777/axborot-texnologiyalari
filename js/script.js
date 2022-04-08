0bc1009ced0ba555e9d73d6132797b2fe2893a75
bobur7777 <azimovbobur09@gmail.com>
2022-04-08 19:37:19 +0500

    added links and some changes

--- js/script.js
+++ js/script.js
@@ -12,21 +12,51 @@ $(document).ready(function () {
   });
   $(".b_maruza").click(function (e) {
     $(".download__1").show(200);
+    $(".download__2").hide(150);
+    $(".download__3").hide(150);
+    $(".download__4").hide(150);
+    $(".download__5").hide(150);
+    $(".download__6").hide(150);
   });
   $(".b_present").click(function (e) {
     $(".download__2").show(200);
+    $(".download__1").hide(150);
+    $(".download__3").hide(150);
+    $(".download__4").hide(150);
+    $(".download__5").hide(150);
+    $(".download__6").hide(150);
   });
   $(".b_video").click(function (e) {
     $(".download__3").show(200);
+    $(".download__1").hide(150);
+    $(".download__2").hide(150);
+    $(".download__4").hide(150);
+    $(".download__5").hide(150);
+    $(".download__6").hide(150);
   });
   $(".b_amaliy").click(function (e) {
     $(".download__4").show(200);
+    $(".download__1").hide(150);
+    $(".download__2").hide(150);
+    $(".download__3").hide(150);
+    $(".download__5").hide(150);
+    $(".download__6").hide(150);
   });
   $(".b_lab").click(function (e) {
     $(".download__5").show(200);
+    $(".download__1").hide(150);
+    $(".download__2").hide(150);
+    $(".download__3").hide(150);
+    $(".download__4").hide(150);
+    $(".download__6").hide(150);
   });
   $(".b_test").click(function (e) {
     $(".download__6").show(200);
+    $(".download__1").hide(150);
+    $(".download__2").hide(150);
+    $(".download__3").hide(150);
+    $(".download__4").hide(150);
+    $(".download__5").hide(150);
   });
 });
 
